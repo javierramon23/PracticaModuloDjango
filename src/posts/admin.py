8f7845f6b6933824d243f03548e37ca9c3c6bfbc
Javier Ramón Sola <javierramon@outlook.com>
2018-03-04 19:09:13 +0100

    Finalizamos la practica con la personalizacion del Administrador de Django.

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
 from posts.models import Category, Post
+from django.utils.safestring import mark_safe
 
 admin.site.register(Category)
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'url', 'summary', 'publish_date')
+
+    list_filter = ('category',)
+
+    def get_image_html(self, post):
+
+        if post.url is not None:
+            return mark_safe('<img src="{0}" alt="{1}" height="150">'.format(post.url, post.title))
+        else:
+            return 'No Image'
+
+    get_image_html.short_description = 'Post Image'
+
+    readonly_fields = ('create_at', 'get_image_html')
+
+    fieldsets = (
+        ('Main Info:', {
+            'fields': ('owner', 'title', 'summary'),
+            'classes': ('collapse',)
+        }),
+        ('Main Content:', {
+            'fields': ('body',),
+            'classes': ('collapse',)
+        }),
+        ('Image:', {
+            'fields': ('get_image_html', 'url',),
+            'description': 'This field is optional',
+            'classes': ('collapse',)
+        }),
+        ('Categories:', {
+            'fields': ('category',),
+            'classes': ('collapse',)
+        }),
+        ('Dates Info:', {
+            'fields': ('publish_date', 'create_at'),
+            'classes': ('collapse',)
+        })
+    )
